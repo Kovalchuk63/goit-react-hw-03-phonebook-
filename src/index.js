4a3a632856d80e0ea0053a829654f523df4be59b
Kovalchuk63 <vov4uk4763@gmail.com>
2023-11-03 20:21:18 +0200

    finish

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import { GlobalStyle } from 'GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import { App } from 'App';
+
+const theme = {
+  bgColorSection: '#a9a9a9',
+  radius: '5px',
+  spacing: value => `${value * 4}px`,
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+      <GlobalStyle />
+    </ThemeProvider>
   </React.StrictMode>
 );
